e6eafb5282810337b475d9d338e7268efd58c63b
Alex_Home <hedonicalex@rocketmail.com>
2018-06-22 00:49:50 +0200

    New project Car.

---
+++ Hello_World/src/Car.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Car {
+
+    Color color;
+    String brand;
+
+    public Car(Color inputColor,String inputBrand){
+
+        this.color=inputColor;
+        this.brand=inputBrand;
+        System.out.println("Print from Car.java. Your car is created");
+
+    }
+
+}
